aee62b8b93bdbcb4ae6c46ef434de399f1424b59
FES001 <firdavs7794948@gmail.com>
2024-03-22 21:15:56 +0500

    s

---
+++ script.js
@@ -0,0 +1,16 @@
+const allLinks = document.querySelectorAll('.links');
+const tracker = document.querySelector('.tracker');
+
+function trackTheLinks(el) {
+    
+
+
+    tracker.style.width = `${el.offsetWidth}px`;
+    tracker.style.left = `${el.offsetLeft}px`;
+}
+
+allLinks.forEach((item, index) => {
+    item.addEventListener('click', e => {
+        trackTheLinks(e.target)
+    });
+})
