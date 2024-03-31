e5862623caa58312962ec9d9e0922664b6dfbb90
FES001 <firdavs7794948@gmail.com>
2024-03-31 16:54:10 +0500

    s

---
+++ projects/learn-from-home/script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu');
+const menuLinks = document.querySelector('.menu-links');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('menu-js');
+
+    menuLinks.classList.toggle('menu-links-js');
+})
